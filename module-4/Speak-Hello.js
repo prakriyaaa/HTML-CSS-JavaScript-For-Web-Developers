9642f3f2d1164abacf6f97dfbfb1b2cccd9bc61a
prakriyaaa <85693469+prakriyaaa@users.noreply.github.com>
2021-06-19 10:43:03 +0545

    Update Speak-Hello.js

--- module-4/Speak-Hello.js
+++ module-4/Speak-Hello.js
@@ -1,37 +1,10 @@
+//Speak-Hello part for module-4
 (function(window) {
-    var speakWord = "Hello";
-    var helloSpeaker = function(name) {
-        console.log(speakWord + " " + name);
-    }
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
 
-    window.helloSpeaker = helloSpeaker;
+	window.helloSpeaker = helloSpeaker;
 
 })(window);
-
-
-
-
-// (function(window) {
-//     var helloSpeaker = new Object();
-//     var speakWord = "Hello";
-//     helloSpeaker.speak = function(name) {
-//         console.log(speakWord + " " + name);
-//     }
-//     window.helloSpeaker = helloSpeaker;
-// })(window);
-
-
-
-
-
-
-// // (function(){
-// //     var helloSpeaker = {};
- 
-// // var speakWord = "Hello";
-// // helloSpeaker.speak = function (name) {
-// //         console.log(speakWord + " " + name);
-// //     };
-
-// // window.helloSpeaker = helloSpeaker;
-// // })(window);
