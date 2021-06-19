2da4b79865c2d5c46855d4927449bc4efc03e172
prakriyaaa <prakriyaadhikari@gmail.com>
2021-06-19 22:00:58 +0545

    My module-4 assignment

--- module-4/Speak-Hello.js
+++ module-4/Speak-Hello.js
@@ -1,37 +1,8 @@
 (function(window) {
-    var speakWord = "Hello";
-    var helloSpeaker = function(name) {
-        console.log(speakWord + " " + name);
-    }
-
-    window.helloSpeaker = helloSpeaker;
-
-})(window);
-
-
-
-
-// (function(window) {
-//     var helloSpeaker = new Object();
-//     var speakWord = "Hello";
-//     helloSpeaker.speak = function(name) {
-//         console.log(speakWord + " " + name);
-//     }
-//     window.helloSpeaker = helloSpeaker;
-// })(window);
-
-
-
-
-
-
-// // (function(){
-// //     var helloSpeaker = {};
- 
-// // var speakWord = "Hello";
-// // helloSpeaker.speak = function (name) {
-// //         console.log(speakWord + " " + name);
-// //     };
-
-// // window.helloSpeaker = helloSpeaker;
-// // })(window);
+  var helloSpeaker = {};
+  helloSpeaker.speak = function(name) {
+    console.log(speakWord + " " + name);
+  }
+  var speakWord = "Hello";
+  window.helloSpeaker = helloSpeaker;
+}(window));
