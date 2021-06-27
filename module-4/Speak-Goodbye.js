074d9e8c110d14420f5a057a6baae4209021f0dd
prakriyaaa <85693469+prakriyaaa@users.noreply.github.com>
2021-06-27 19:37:07 +0545

    Update Speak-Goodbye.js

--- module-4/Speak-Goodbye.js
+++ module-4/Speak-Goodbye.js
@@ -1,19 +1,8 @@
 (function(window) {
-<<<<<<< HEAD
   var byeSpeaker = {};
   byeSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
   }
   var speakWord = "Good Bye";
   window.byeSpeaker = byeSpeaker;
-}(window));
-=======
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
-})(window);
->>>>>>> 2d7f53a26770046c4b889737a80d5a378b9e98fe
+}(window))
