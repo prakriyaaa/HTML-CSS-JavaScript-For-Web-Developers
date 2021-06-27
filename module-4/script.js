3521974f94d72a6f14a59657dd20158576f72054
prakriyaaa <85693469+prakriyaaa@users.noreply.github.com>
2021-06-27 19:41:46 +0545

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-(function() {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
-    }
-  }
-})();
-=======
-//Script.js part of module-4
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
@@ -28,4 +14,3 @@ for (var i = 0; i < names.length; i++) {
 }
 
 })();
->>>>>>> 2d7f53a26770046c4b889737a80d5a378b9e98fe
