c4a13d39a6d672a50291d3aa4eaa9ae5b34d6a0b
prakriyaaa <prakriyaadhikari@gmail.com>
2021-06-27 19:58:39 +0545

    my fourth assignment

--- module-4/Speak-Goodbye.js
+++ module-4/Speak-Goodbye.js
@@ -1,5 +1,4 @@
 (function(window) {
-<<<<<<< HEAD
   var byeSpeaker = {};
   byeSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
@@ -7,13 +6,4 @@
   var speakWord = "Good Bye";
   window.byeSpeaker = byeSpeaker;
 }(window));
-=======
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
 
-})(window);
->>>>>>> 2d7f53a26770046c4b889737a80d5a378b9e98fe
--- module-4/Speak-Hello.js
+++ module-4/Speak-Hello.js
@@ -1,6 +1,4 @@
-//Speak-Hello part for module-4
 (function(window) {
-<<<<<<< HEAD
   var helloSpeaker = {};
   helloSpeaker.speak = function(name) {
     console.log(speakWord + " " + name);
@@ -8,13 +6,3 @@
   var speakWord = "Hello";
   window.helloSpeaker = helloSpeaker;
 }(window));
-=======
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
-})(window);
->>>>>>> 2d7f53a26770046c4b889737a80d5a378b9e98fe
